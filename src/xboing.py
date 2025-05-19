a3958dada09255171b7b0599f43ed7a8e4219588
jmf-pobox <155204947+jmf-pobox@users.noreply.github.com>
2025-05-18 21:39:59 -0700

    chore(logging): change logging level to INFO

--- src/xboing.py
+++ src/xboing.py
@@ -44,7 +44,7 @@ from utils.asset_paths import get_asset_path, get_sounds_dir
 from utils.logging_config import setup_logging
 
 # Setup logging
-setup_logging(logging.DEBUG)
+setup_logging(logging.INFO)
 
 # Game constants - matching the original XBoing dimensions
 PLAY_WIDTH: int = 495  # Original game's play area width
